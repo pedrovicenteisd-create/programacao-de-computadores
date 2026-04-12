1c1bdbe411d0989e1f741078aba9e22cb49130ad
Pedro Vicente <pedrovicenteisd@gmail.com>
2026-04-12 02:33:22 -0300

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,9 @@
+x = int(input("Digite o valor de x: ")
+if(x%5==0):
+        print("Múltiplo de 5")
+elif(x%5!==0):
+        print("Não é múltiplo")
+elif(x>50):
+  print("Múltiplo alto")
+else:
+  print("Múltiplo baixo")
