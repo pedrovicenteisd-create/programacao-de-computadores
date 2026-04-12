331c7cca507227d9380be3f6214357172d0e396c
Pedro Vicente <pedrovicenteisd@gmail.com>
2026-04-12 00:27:50 -0300

    Create 24. Leia um número inteiro. Verifique se ele está no intervalo de 1 a 100 (inclusive). Se estiver: Verifique se o número é par ou ímpar; Exiba: “Par no intervalo” ou “Ímpar no intervalo”. Caso não esteja no intervalo, exiba: “Fora do intervalo”.py

---
+++ 24. Leia um número inteiro. Verifique se ele está no intervalo de 1 a 100 (inclusive). Se estiver: Verifique se o número é par ou ímpar; Exiba: “Par no intervalo” ou “Ímpar no intervalo”. Caso não esteja no intervalo, exiba: “Fora do intervalo”.py
@@ -0,0 +1,7 @@
+x=int(input("Digite o valor de x: "))
+if(0<=x) and(x<=100) and(x%2==0):
+      print("par no intervalo")
+elif(0<=x) and(x<=100)and(x%2==1):
+      print("impar no intervalo")
+else:
+     print("Fora do intervalo")
